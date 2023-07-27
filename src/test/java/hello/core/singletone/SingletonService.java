9f2ebe029253739c97a75d6fa098a5077bfd04d7
SB8919-LAPTOP\sb891 <sb8919@naver.com>
2023-07-27 16:17:53 +0900

    section5 complete

---
+++ src/test/java/hello/core/singletone/SingletonService.java
@@ -0,0 +1,16 @@
+package hello.core.singletone;
+
+public class SingletonService {
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance(){
+        return instance;
+    }
+    private SingletonService(){
+
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 로직 호출");
+    }
+}
